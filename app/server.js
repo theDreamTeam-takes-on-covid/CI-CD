d029e994cc30429eb4df97799fdd8aed59f45c71
Kato Agaba Arnold <arnoldkatoagaba@gmail.com>
2021-09-20 12:54:12 +0300

    update 1.0

--- app/server.js
+++ app/server.js
@@ -24,7 +24,7 @@ app.get('/', (req,res) => {
     res.status(404).send('404! This is an invalid URL.')
   });
 
-  
+  //app.listen(3005, ()=> {console.log('Example app listening on port 3005!')});
 
   module.exports = app.listen(3005, ()=> {console.log('Example app listening on port 3005!')});
 
