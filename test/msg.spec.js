079a0acd56580eb8a803ed09b75f1e524720bcc0
Kato Agaba Arnold <arnoldkatoagaba@gmail.com>
2021-09-21 14:34:57 +0300

    NEW UPDATE 4.0

--- test/msg.spec.js
+++ test/msg.spec.js
@@ -1,6 +1,6 @@
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-let server = require("../../../server");
+let server = require("../server");
 const { response } = require("express");
 
 //assertion Style
